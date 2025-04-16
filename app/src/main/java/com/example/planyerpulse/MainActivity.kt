f50143ea1b4d35f021a45616874e6e50d4a0a0cd
sapounasGH <154423884+sapounasGH@users.noreply.github.com>
2025-04-16 16:39:45 +0300

    no message

--- app/src/main/java/com/example/planyerpulse/MainActivity.kt
+++ app/src/main/java/com/example/planyerpulse/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             PlanyerPulseTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "AndroidSapounas",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
