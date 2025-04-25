796d9bf3a4d90beec1d156f73a47e07a2f1e43c0
sapounasGH <154423884+sapounasGH@users.noreply.github.com>
2025-04-25 14:55:48 +0300

    no message

--- app/src/main/java/com/example/planyerpulse/drasidetails.java
+++ app/src/main/java/com/example/planyerpulse/drasidetails.java
@@ -13,6 +13,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -28,15 +29,18 @@ public class drasidetails extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        MapView mapView = findViewById(R.id.map);
-        mapView.onCreate(savedInstanceState);
-        mapView.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(@NonNull GoogleMap googleMap) {
-                LatLng thessaloniki = new LatLng(40.6401, 22.9444);
-                googleMap.addMarker(new MarkerOptions().position(thessaloniki).title("Thessaloniki"));
-                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(thessaloniki, 12f));
-            }
-        });
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(new OnMapReadyCallback() {
+                @Override
+                public void onMapReady(@NonNull GoogleMap googleMap) {
+                    LatLng thessaloniki = new LatLng(40.6401, 22.9444);
+                    googleMap.addMarker(new MarkerOptions().position(thessaloniki).title("Thessaloniki"));
+                    googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(thessaloniki, 12f));
+                }
+            });
+        }
     }
 }
