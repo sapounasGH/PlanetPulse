d7325e40be16857a64c6e606a58af8d21bbc945e
mariaen1 <mariaenechelidou@gmail.com>
2025-05-23 17:01:23 +0300

    hy

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,10 @@
+
+
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    alias(libs.plugins.gms)
+
 }
 
 android {
@@ -9,15 +13,13 @@ android {
 
     defaultConfig {
         applicationId = "com.example.planyerpulse"
-        minSdk = 34
+        minSdk = 26
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
+
     }
 
     buildTypes {
@@ -30,44 +32,38 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
+
 }
 
 dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
+
     implementation(libs.material)
+
+
     implementation(libs.androidx.activity)
+
     implementation(libs.androidx.constraintlayout)
     implementation("com.google.android.gms:play-services-maps:19.2.0")
+
+
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.firestore)
+
+
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+
+
 }
--- app/src/main/java/com/example/planyerpulse/LoginActivity.kt
+++ app/src/main/java/com/example/planyerpulse/LoginActivity.kt
@@ -1,11 +1,16 @@
 package com.example.planyerpulse
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+
 
 class LoginActivity : AppCompatActivity() {
 
@@ -18,5 +23,14 @@ class LoginActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val loginButton = findViewById<Button>(R.id.loginSubmitButton)
+
+        loginButton.setOnClickListener {
+            val username = findViewById<EditText>(R.id.usernameField).text.toString().trim()
+            val password = findViewById<EditText>(R.id.passwordField).text.toString().trim()
+
+
+        }
+
     }
 }
